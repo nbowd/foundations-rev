34c2f14e04b1b8b2474ff1c39abd141465bf59a6
nbowd <nickbwdn@gmail.com>
2025-03-12 12:52:42 -0700

    Added logger middleware using winston

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
-const app = express();
-
 const bodyParser = require('body-parser');
+const logger = require('./utils/logger');
 
 // Routes
 const usersController = require('./controller/usersController');
 const loginController = require('./controller/loginController');
 const ticketsController = require('./controller/ticketsController');
 
+function loggerMiddleware(req, res, next){
+    logger.info(`Incoming ${req.method} : ${req.url}`);
+    next();
+}
+
+const app = express();
 app.use(bodyParser.json());
+app.use(loggerMiddleware);
 
 app.use('/users', usersController);
 app.use('/login', loginController);
